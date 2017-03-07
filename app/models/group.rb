c0269797c59d3788dbe02f496224bdf3d1250439
ypj <ypj2005@126.com>
2017-03-07 22:10:59 +0800

    implement group ralation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, presence: true
+  has_many :group_relationships
+  has_many :member, through: :group_relationships, source: :user
 end
